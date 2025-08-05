0eec18c291e6cbbbc3ee2143597290f2b02bf7af
luminus <nurudeenexpert@gmail.com>
2025-08-05 13:55:23 +0100

    function for gameRound added

--- index.js
+++ index.js
@@ -30,4 +30,48 @@ function getHumanChoice() {
     }
 }
 
+function gameRound(humanChoice,computerChoice) {
+    if (humanChoice === "Rock" && computerChoice === "Rock") {
+        alert ("Rock vs Rock -> It's a tie!");
+        console.log ("Rock vs Rock -> It's a tie!");
+    }
+    else if (humanChoice === "Rock" && computerChoice === "Paper") {
+        alert ("Rock vs Paper -> You lost this round!");
+        console.log ("Rock vs Paper -> You lost this round!");
+        computerScore++ ;
+    }
+    else if (humanChoice === "Rock" && computerChoice === "Scissor") {
+        alert ("Rock vs Scissor -> You won this round!");
+        console.log ("Rock vs Scissor -> You won this round!");
+        humanScore++ ;
+    }
+    else if (humanChoice === "Paper" && computerChoice === "Paper") {
+        alert ("Paper vs Paper -> It's a tie!");
+        console.log ("Paper vs Paper -> It's a tie!");
+    }
+    else if (humanChoice === "Paper" && computerChoice === "Scissor") {
+        alert ("Paper vs Scissor -> You lost this round!");
+        console.log ("Paper vs Scissor -> You lost this round!");
+        computerScore++ ;
+    }
+    else if (humanChoice === "Paper" && computerChoice === "Rock") {
+        alert ("Paper vs Rock -> You won this round!");
+        console.log ("Paper vs Rock -> You won this round!");
+        humanScore++ ;
+    }
+    if (humanChoice === "Scissor" && computerChoice === "Scissor") {
+        alert ("Scissor vs Scissor -> It's a tie!");
+        console.log ("Scissor vs Scissor -> It's a tie!");
+    }
+    else if (humanChoice === "Scissor" && computerChoice === "Rock") {
+        alert ("Scissor vs Rock -> You lost this round!");
+        console.log ("Scissor vs Rock -> You lost this round!");
+        computerScore++ ;
+    }
+    else {
+        alert ("Scissor vs Paper -> You won this round!");
+        console.log ("Scissor vs Paper -> You won this round!");
+        humanScore++ ;
+    }
 
+}
