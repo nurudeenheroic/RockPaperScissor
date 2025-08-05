a0a005ab6fe288bf607d61824300289210f70e7d
luminus <nurudeenexpert@gmail.com>
2025-08-05 13:13:50 +0100

    function for computer choice added

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 let humanScore = 0, computerScore = 0;
+
+function getComputerScore() {
+    let computerChoice = Math.floor(Math.random()*3);
+
+    switch (computerChoice) {
+        case 0: 
+            return 'Rock';
+        case 1:
+            return 'Paper';
+        default:
+            return 'Sccissor';
+    }
+}
